7428a83b75eb26477b510300be3f0e4b8f1e8223
Jason Kim <95jasonkim@gmail.com>
2021-02-05 13:47:50 -0900

    Implemented function myStrdup

--- Hw/Hw1/c_style_strings/c_style_strings.cpp
+++ Hw/Hw1/c_style_strings/c_style_strings.cpp
@@ -3,19 +3,41 @@ Jason Kim
 CS202 Hw1
 c_style_strings
 */
+#include<iostream>
+using std::cout;
+using std::endl;
 
-
-char* strdup(const char*)
+char* myStrdup(const char* x)
 {
-
+	int length = 0;
+	while (*x)
+	{
+		x++;
+		length++;
+	}
+	x -= length;
+	char* copy = new char[length];
+	for (int i = 0; i < length; i++)
+	{
+		*copy = *x;
+		copy++;
+		x++;
+	}
+	*copy = '\0';
+	copy -= length;
+	return copy;
 }
 
-char* findx(const char* s, const char* x)
-{
-
-}
+//char* findx(const char* s, const char* x)
+//{
+//
+//}
 
 int main()
 {
+	//from my modern world history textbook
+	const char* text = "The Korean conflict was the first major military battle of the Cold War between democracy and communism.";
+	auto test = myStrdup(text);
+	cout << test;
 
 }
