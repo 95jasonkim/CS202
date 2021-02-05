47f31d2b3157a3cdc2459ca3ebc944c90c523d22
Jason Kim <95jasonkim@gmail.com>
2021-02-05 04:42:24 -0900

    Implemented constructor for StopWatch

--- Hw/Hw1/TimeIt1/StopWatch.cpp
+++ Hw/Hw1/TimeIt1/StopWatch.cpp
@@ -4,3 +4,9 @@ StopWatch class cpp file
 */
 
 #include "StopWatch.hpp"
+using std::chrono::high_resolution_clock;
+
+StopWatch::StopWatch():_startTime(high_resolution_clock::now())
+{}
+
+
--- Hw/Hw1/TimeIt1/TimeIt1.cpp
+++ Hw/Hw1/TimeIt1/TimeIt1.cpp
@@ -2,5 +2,12 @@
 Jason Kim
 CS202 Time it 1
 */
-
 #include "StopWatch.hpp"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+}
