eb3f3e1afb6dc1e9359abab2e0e5049591ddd40e
Jason Kim <95jasonkim@gmail.com>
2021-02-12 19:24:26 -0900

    Added files and catch 2 for project

---
+++ Hw/Hw2/LinkedList/linkedlist.cpp
@@ -0,0 +1,8 @@
+/*
+Jason Kim
+CS202 Hw2
+Linked List
+*/
+
+#include"catch_amalgamated.hpp"
+
---
+++ Hw/Hw2/LinkedList/main.cpp
@@ -0,0 +1,7 @@
+/*
+Jason Kim
+CS202 Hw2 
+main.cpp for catch2 test
+*/
+#define CATCH_CONFIG_MAIN
+#include"catch_amalgamated.hpp"
