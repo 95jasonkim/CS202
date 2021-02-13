60d17d7bd983850ba7b8e102511f3f9f55ddd108
Jason Kim <95jasonkim@gmail.com>
2021-02-12 20:16:15 -0900

    Created file and added printPointer function

---
+++ Hw/Hw2/Memorylayout/memorylayout.cpp
@@ -0,0 +1,20 @@
+/*
+Jason Kim
+CS202 hw2
+Memory Layout
+*/
+#include<iostream>
+using std::cout;
+using std::endl;
+
+void printPointer(int i)
+{
+	int* iptr = &i;
+	cout << "i = " << *iptr << endl;
+	cout << "&i = " << iptr << endl;
+}
+
+int main()
+{
+
+}
