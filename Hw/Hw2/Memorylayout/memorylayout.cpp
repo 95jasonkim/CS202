49cbeb7cad2e61fc775f9898098f390df19996a9
Jason Kim <95jasonkim@gmail.com>
2021-02-12 22:13:18 -0900

    added code to show addresses for memory types

--- Hw/Hw2/Memorylayout/memorylayout.cpp
+++ Hw/Hw2/Memorylayout/memorylayout.cpp
@@ -7,7 +7,7 @@ Memory Layout
 using std::cout;
 using std::endl;
 
-void printPointer(int i)
+void printPointer(int& i)
 {
 	int* iptr = &i;
 	cout << "i = " << *iptr << endl;
@@ -16,5 +16,33 @@ void printPointer(int i)
 
 int main()
 {
+	cout << "stack storage: " << endl;
+	int stack =1;
+	printPointer(stack);
+	int stack2 = 2;
+	printPointer(stack2);
+	cout << endl;
 
+	cout << "static storage: " << endl;
+	static int myStatic = 3;
+	static int myStatic2 = 4;
+	printPointer(myStatic);
+	printPointer(myStatic2);
+	cout << endl;
+
+	cout << "free store: " << endl;
+	int *free = new int(5);
+	int *free2 = new int(6);
+	printPointer(*free);
+	printPointer(*free2);
+	cout << endl;
+
+	cout << "free store array: " << endl;
+	int size = 4;
+	int* freeArr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		freeArr[i] = i + 1;
+		printPointer(freeArr[i]);
+	}
 }
