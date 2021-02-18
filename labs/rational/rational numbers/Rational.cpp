e8792e2aad6638bc27a11292a9e6ff6e0692cdb4
Jason Kim <95jasonkim@gmail.com>
2021-02-18 10:56:28 -0900

    rational

--- labs/rational/rational numbers/Rational.cpp
+++ labs/rational/rational numbers/Rational.cpp
@@ -3,3 +3,23 @@ Jason Kim
 CS202 rational class cpp
 */
 #include "Rational.hpp"
+
+Rational::Rational(int num, int den) : _numerator(num), _denominator(den) {
+}
+
+std::ostream& operator<<(std::ostream& os, const Rational& rhs) {
+	return os << rhs._numerator << "/" << rhs._denominator;
+}
+
+Rational operator+(const Rational& lhs, const Rational& rhs) { //canonical
+   //a/b + c/d = (ad+bc)/ad
+	auto temp{ lhs };
+	temp += rhs;
+	return temp;
+}
+
+Rational& Rational::operator+=(const Rational& rhs) {
+	_numerator = _numerator * rhs._denominator + rhs._numerator * _denominator;
+	_denominator *= rhs._denominator;
+	return *this;
+}
--- labs/rational/rational numbers/Rational.hpp
+++ labs/rational/rational numbers/Rational.hpp
@@ -2,10 +2,20 @@
 Jason Kim
 CS202 rational class hpp
 */
-class Rational
-{
+#ifndef RATIONAL_HPP
+#define RATIONAL_HPP
+#include<iostream>
+
+class Rational {
+	friend std::ostream& operator<<(std::ostream&, const Rational& rhs);
+	friend Rational operator+(const Rational& lhs, const Rational& rhs);
 public:
+	Rational(int, int = 1);
+	Rational& operator+=(const Rational& rhs);
 private:
-	int _nume, _deno;
+	int _numerator;
+	int _denominator;
 };
 
+
+#endif
