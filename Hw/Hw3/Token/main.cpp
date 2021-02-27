4a66e2c14ec1ed7ede4873596c2f76fba22d0495
Jason Kim <95jasonkim@gmail.com>
2021-02-26 20:05:49 -0900

    added code to take time and test

--- Hw/Hw3/Token/main.cpp
+++ Hw/Hw3/Token/main.cpp
@@ -16,5 +16,12 @@ int main(int argc, char** argv)
 {
 	string name = argv[1];
 	ifstream ifs(name);
+	/* code for taking time
+	auto start = std::chrono::high_resolution_clock::now();
+	auto test = readLines(ifs);
+	auto stop = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	cout << duration.count() << " microseconds" << endl;
+	*/
 	printTokens(cout, readLines(ifs));
 }
