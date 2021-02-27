87bfeede1cd42738589242ffb7451acdacbec481
Jason Kim <95jasonkim@gmail.com>
2021-02-26 20:27:13 -0900

    added << operator for Money

--- Hw/Hw3/money/Money.cpp
+++ Hw/Hw3/money/Money.cpp
@@ -3,6 +3,7 @@ Jason Kim
 CS202 hw3 money class cpp
 */
 #include "Money.hpp"
+#include<iomanip>
 
 Money::Money()
 {
@@ -14,4 +15,15 @@ Money::Money(int amount): _amount { amount }
 
 Money::Money(const Money& M) : _amount{ M._amount }
 {
+}
+
+std::ostream& operator<<(std::ostream& os, const Money& M)
+{
+	double dollars = M._amount / 100.0;
+	if (dollars < 0)
+	{
+		dollars *= -1;
+		return os << "-$" << std::fixed << std::setprecision(2) << dollars;
+	}
+	else return os << "$" << std::fixed << std::setprecision(2)<< dollars;
 }
