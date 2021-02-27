36d336ddec9737d3f55cdb7eb366162c38404911
Jason Kim <95jasonkim@gmail.com>
2021-02-26 19:30:29 -0900

    implemented function lineToTokens

--- Hw/Hw3/Token/main.cpp
+++ Hw/Hw3/Token/main.cpp
@@ -5,8 +5,27 @@ cs202 hw3 token main
 #include"token.hpp"
 using std::cout;
 using std::endl;
+#include<fstream>
+using std::ifstream;
+using std::string;
+using std::getline;
+using std::vector;
 
-int main()
+int main(int argc, char** argv)
 {
-
+	string name = argv[1];
+	ifstream ifs(name);
+	string line;
+	if (ifs.is_open())
+	{
+		while (getline(ifs, line))
+		{
+			vector<string> s = lineToTokens(line);
+			for (auto item : s)
+			{
+				cout << item << endl;
+			}
+		}
+		ifs.close();
+	}
 }
--- Hw/Hw3/Token/token.cpp
+++ Hw/Hw3/Token/token.cpp
@@ -2,8 +2,35 @@
 Jason kim
 cs202 hw3 token
 */
+#include"token.hpp"
 #include<iostream>
-using std::string;
+#include<string>
 using std::getline;
+using std::string;
+#include<vector>
+using std::vector;
+#include<sstream>
+using std::stringstream;
+
+vector<string> lineToTokens(const std::string& line)
+{
+	stringstream ss(line);
+	string word;
+	vector<string> tokens;
+	while (ss)
+	{
+		ss >> word;
+		if (ss)
+		{
+			tokens.push_back(word);
+		}
+	}
+	return tokens;
+}
+
+vector<TokenAndPosition> readLines(std::istream& is);
+
+void printTokens(std::ostream& os, const vector<TokenAndPosition>& tokens);
+
 
 
