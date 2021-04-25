61464930e44ec01674232543b60f9fa874706b4f
Jason Kim <95jasonkim@gmail.com>
2021-04-24 21:14:33 -0800

    function fib_loop without recursion

--- Hw/Hw5/Recursion/recursion.cpp
+++ Hw/Hw5/Recursion/recursion.cpp
@@ -11,8 +11,24 @@ int fib(int n)
 	return fib(n - 2) + fib(n - 1);
 }
 
+int fib_loop(int n)
+{
+	int j=0;
+	int k=1;
+	int h;
+	for (int i = 0; i<=n ; i++)
+	{
+		if (i > 1)
+		{
+			h = j + k;
+			j = k;
+			k = h;
+		}
+	}
+	return h;
+}
 
 int main()
 {
-	cout << fib(7) << endl;
+	cout << fib_loop(7) << endl;
 }
